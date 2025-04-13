8a2a4a2265487da752d319cf6596594512f2252d
muneershaik <muneer509@gmail.com>
2025-04-13 17:39:05 +0530

    shell

--- 02.conversion.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Hi, Meera"
-echo "Hello, Muneer"
-echo "Meera: how is your DEVOPS practice?"
-echo "Muneer: im practice shell in DEVOPS now"
--- 03.variable.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-name1="Muneer"
-name2="Meera"
-
-echo "Hi, $name2"
-echo "Hello, $name1"
-echo "$name2: how is your DEVOPS practice?"
-echo "$name1: im practice shell in DEVOPS now"
