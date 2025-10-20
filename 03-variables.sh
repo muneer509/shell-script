99efd519bb68c78e5fdaba5c91102bb7ac39fae7
Muneer Shaik <mshaik4@berkadia.com>
2025-10-20 18:04:14 +0530

    coversion

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-$p1=Ramesh
-$p2=Suresh
-echo "$p1: Hi..$p2"
+p1=Ramesh
+p2=Suresh
+echo "${p1}: Hi..$p2"
 echo "$p2: Hi..$p1"
 echo "$p1: how are you $p2?"
 echo "$p2: Fine, thank you for asking $p1"
