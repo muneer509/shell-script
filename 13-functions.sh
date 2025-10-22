04b5c6cd3599e0f58c6e0a05a8f6a901bfaf15a1
Muneer Shaik <mshaik4@berkadia.com>
2025-10-22 19:15:21 +0530

    practice

--- 13-functions.sh
+++ 13-functions.sh
@@ -4,3 +4,18 @@ if [ $root -ne 0 ]
 then
     echo "Please try with Root access"
 fi
+apt list --installed | grep -i git
+if [ $? -ne 0 ]
+then    
+    echo "Git not available, so installing it"
+    apt install git -y
+    if [ $? -ne 0 ]
+    then    
+        echo "git stallation failed:"
+        exit 1
+    else
+        echo "Git installation success"
+    fi
+else
+    echo "Git already installled"
+fi
