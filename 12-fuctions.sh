27e9441a71a97db6854ac5c8c253f186a46bddbd
Muneer Shaik <mshaik4@berkadia.com>
2025-10-22 16:44:56 +0530

    practice

--- 12-fuctions.sh
+++ 12-fuctions.sh
@@ -1,22 +1,45 @@
 #!/bin/bash
-ROOT=$(id -u)
 
-if [ $ROOT -ne 0 ]
-then
-    echo "Please run with root access"
-    exit 1
-fi
-installation_validation() {
+USERID=$(id -u)
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run this script with root priveleges"
+        exit 1
+    fi
+}
+
+VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2 already installed:"
+        echo "$2 is...FAILED"
+        exit 1
+    else
+        echo "$2 is...SUCCESS"
     fi
-        }
+}
+
+CHECK_ROOT
 
 dnf list installed git
 
- 
-installation_validation $? "Git"
-dnf list installed nginx
-installation_validation $? "Nginx"
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed, going to install it.."
+    dnf install git -y
+    VALIDATE $? "Installing Git"
+else
+    echo "Git is already installed, nothing to do.."
+fi
 
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed...going to install"
+    dnf install mysql -y
+    VALIDATE $? "Installing MySQL"
+else
+    echo "MySQL is already installed..nothing to do"
+fi
