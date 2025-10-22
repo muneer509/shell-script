9683b2da23241286f529d68d294822373b268c15
Muneer Shaik <mshaik4@berkadia.com>
2025-10-22 16:03:47 +0530

    practice

--- 11-installation.sh
+++ 11-installation.sh
@@ -6,6 +6,32 @@ then
     echo "Please run with root access"
     exit 1
 fi
-
-apt update &&
-apt install mysql-server 
+dnf list installed git
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed, going to install it.."
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then 
+        echo "Git installation faied"
+        exit 1
+    else
+        echo "Git installation success"
+else
+    echo "Git is already installed"
+fi
+dnf list installed mysql-server
+if [ $? -ne 0 ]
+then    
+    echo "Installing mysql-server"
+    dnf install mysql-server -y
+    if [ $? -ne 0 ]
+    then    
+        echo "mysql installation failed"
+        exit 1
+    else
+        echo "mysql installation sucess"
+    fi
+else
+    echo "mysql already installed"
+fi
