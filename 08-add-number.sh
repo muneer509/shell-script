83b968e53465cfc9283a70298a4b3ddc08b2f0b1
Muneer Shaik <mshaik4@berkadia.com>
2025-10-22 14:35:13 +0530

    practice

--- 08-add-number.sh
+++ 08-add-number.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
-echo "enter a value"
-read a
-echo "enter b value"
-read b
-echo "total is: $((a+b))"
+# #!/bin/bash
+# echo "enter a value"
+# read a
+# echo "enter b value"
+# read b
+# echo "total is: $((a+b))"
+
+#!bin/bash
+echo "a value is:$1
+echo "b value is:$2
+echo "total is:$((($1)+($2))"
