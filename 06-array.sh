d70a97357da250c6f809e2b05db1e58462fd196f
Muneer Shaik <mshaik4@berkadia.com>
2025-10-22 14:16:36 +0530

    practice

--- 06-array.sh
+++ 06-array.sh
@@ -1,16 +1,21 @@
+# #!/bin/bash
+# names=("Muneer" "Shameer" "haseena" "Karim")
+# echo "Father name is ${names[3]}"
+# echo "Mother name is ${names[2]}"
+# echo "all family members names are ${names[@]}"
+
+# #!/bin/bash
+
+# # index starts from 0, size is 3
+# # FRUITS=("APPLE" "KIWI" "ORANGE") #Array
+
+# # echo "First fruit is: ${FRUITS[0]}"
+# # echo "Second fruit is: ${FRUITS[1]}"
+# # echo "Third fruit is: ${FRUITS[2]}"
+
+# # echo "First fruit is: ${FRUITS[@]}"
 #!/bin/bash
-names=("Muneer" "Shameer" "haseena" "Karim")
-echo "Father name is ${names[3]}"
-echo "Mother name is ${names[2]}"
-echo "all family members names are ${names[@]}"
-
-#!/bin/bash
-
-# index starts from 0, size is 3
-# FRUITS=("APPLE" "KIWI" "ORANGE") #Array
-
-# echo "First fruit is: ${FRUITS[0]}"
-# echo "Second fruit is: ${FRUITS[1]}"
-# echo "Third fruit is: ${FRUITS[2]}"
-
-# echo "First fruit is: ${FRUITS[@]}"
+family=("Father","Mother","Brother","Sister")
+echo "${family(0)}"
+echo "${family(1)}"
+echo "${family(2)}"
