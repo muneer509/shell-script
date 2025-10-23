85800f57d615fef765d3b64730674c060ff09ff7
Muneer Shaik <mshaik4@berkadia.com>
2025-10-23 14:53:36 +0530

    practice

---
+++ 14-package-installation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+id=${id -u}
+if [ $id -ne 0 ]
+then
+    echo "Please try with Root"
+fi
+validate() {
+    if [ $1 -ne 0 ]
+    then    
+        echo "$2 is failed"
+    else
+        echo "$2 is sucess"
+}
+for i in $@ # all args passed to script
+do
+    echo $i
+done
