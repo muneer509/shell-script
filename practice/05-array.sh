69d33b2a3c5ef2847680d654b5803e1b66b2a2db
Muneer Shaik <mshaik4@berkadia.com>
2025-11-12 11:30:41 +0530

    shell

--- practice/05-array.sh
+++ practice/05-array.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# index starts from 0, size is 3
-FRUITS=("APPLE" "KIWI" "ORANGE") #Array
+names=("Muneer" "Haseena" "Jani" "Azmath")
 
-echo "First fruit is: ${FRUITS[0]}"
-echo "Second fruit is: ${FRUITS[1]}"
-echo "Third fruit is: ${FRUITS[2]}"
-
-echo "First fruit is: ${FRUITS[@]}"
+echo "Mother name is: ${names[1]}"
+echo "Names are: ${names[@]}"
