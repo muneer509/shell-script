18fc95e32ad899f25b7c58679e3cbf42e615a412
Muneer Shaik <mshaik4@berkadia.com>
2025-11-12 11:25:33 +0530

    shell

--- practice/05-array.sh
+++ practice/05-array.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-names=("Muneer" "Shameer" "Haseena" "Jani")
 
-echo "father name is ${names[3]}"
+# index starts from 0, size is 3
+FRUITS=("APPLE" "KIWI" "ORANGE") #Array
+
+echo "First fruit is: ${FRUITS[0]}"
+echo "Second fruit is: ${FRUITS[1]}"
+echo "Third fruit is: ${FRUITS[2]}"
+
+echo "First fruit is: ${FRUITS[@]}"
