6862c73563a8b2044a6573a11f4ee9562491d304
Muneer Shaik <mshaik4@berkadia.com>
2025-11-12 09:59:12 +0530

    shell

--- practice/04-userIDpassword.sh
+++ practice/04-userIDpassword.sh
@@ -1,5 +1,5 @@
 echo "enter user ID to login"
 read  userid
 echo "enter the password:"
-read  pwd 
-echo "User ID is $userid and password is: $pwd
+read  -s pwd 
+echo "User ID is $userid and password is: $pwd"
